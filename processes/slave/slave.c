1fa0cd7f1793df6b008c43a1164f092e8dbe02e5
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2024-04-07 03:36:59 -0300

    Slave now can receive from stdin the files´ path

--- processes/slave/slave.c
+++ processes/slave/slave.c
@@ -2,62 +2,68 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <sys/select.h>
 
 //#define PID_ERROR (-1)
 //#define PARENT_PID 0                                             //The pid 0 refers to the parent process but the parent pid is stored by the child
 //#define EXECVE_ERROR (-1)
 
+#define ERROR (-1)
 
-
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 256                                             //The buffer size is bigger than necessary to avoid overflowing the buffer (It will be considered that every fgets will not reach the last character of the BUFFER_SIZE)
 
 FILE *popen(const char *command, const char *type);                 //Must be included to avoid warnings
 int pclose(FILE *stream);
 
-void md5sum_caller(char* path, char* buffer);
+static void exitWithFailure(const char *errMsg);
 
-int main(int argc, char* argv[]) {
-    for (int i = 1; argv[i]!=NULL; i++) {                                       //The slave will process a number of files sended by the master
-        char buffer[BUFFER_SIZE]={'\0'};
-        md5sum_caller(argv[i], buffer);
-        buffer[strlen(buffer)-1]='\0';                                      //I remove the newline created by fgets
-        printf("%s - PROCESS PID %d\n",buffer, getpid());
+void md5sum_caller(char *path, char *buffer);
+
+int main(int argc, char *argv[]) {
+    int eof_flag = 0;
+    fd_set fdSet;
+    while (!eof_flag) {                                                                              //The slave will process a number of files sent by the master one at the time, so with select I avoid busy waiting
+        FD_ZERO(&fdSet);
+        FD_SET(STDIN_FILENO, &fdSet);
+        int val = select(1, &fdSet, NULL, NULL, NULL);
+        if (val == ERROR)
+            exitWithFailure("SELECT didn´t work as expected");
+        if (val) {
+            char path[BUFFER_SIZE];
+            if (fgets(path, BUFFER_SIZE, stdin) == NULL) {
+                if (feof(stdin))
+                    eof_flag = 1;
+                else if (ferror(stdin))
+                    exitWithFailure("FGETS was unsuccessful to read the data into the buffer");
+            } else {
+                path[strlen(path)-1]='\0';
+                char buffer[BUFFER_SIZE] = {'\0'};
+                md5sum_caller(path, buffer);
+                buffer[strlen(buffer) -1] = '\0';                                      //I remove the newline created by fgets
+                printf("%s - PROCESS PID %d\n", buffer, getpid());
+            }
+        }
     }
 }
 
-void md5sum_caller(char* path, char* buffer) {
-    char md5sum_command[BUFFER_SIZE]="md5sum ";                                                 //The md5sum call to be made
-    strcpy(md5sum_command+ strlen(md5sum_command), path );                  //Add the file path to the md5sum_command
-    FILE *fp = popen(md5sum_command, "r");                              //Popen makes the fork call and creates the pipe, r means reading mode
-    if( fp == NULL ){                                                                   //check Popen failure
-        perror("POPEN couldn´t be created successfully");
-        exit(EXIT_FAILURE);
-    }
-    if(fgets(buffer, BUFFER_SIZE, fp)==NULL) {                           //Fgets fills the buffer with the md5sum result
-        perror("FGETS was unsuccessful to read the data into the buffer");         //Error handling
-        exit(EXIT_FAILURE);
-    }
-    if(pclose(fp)==-1){
-        perror("PCLOSE couldn´t close successfully");
-        exit(EXIT_FAILURE);
-    };
+void md5sum_caller(char *path, char *buffer) {
+    char md5sum_command[BUFFER_SIZE] = "md5sum ";                                                 //The md5sum call to be made
+    strcpy(md5sum_command + strlen(md5sum_command),
+           path);                  //Add the file path to the md5sum_command
+    FILE *fp = popen(md5sum_command,
+                     "r");                              //Popen makes the fork call and creates the pipe, r means reading mode
+    if (fp == NULL)                                                                  //check Popen failure
+        exitWithFailure("POPEN couldn´t be created successfully");
+    if (fgets(buffer, BUFFER_SIZE, fp) ==
+        NULL)                            //Fgets fills the buffer with the md5sum result
+        exitWithFailure("FGETS was unsuccessful to read the data into the buffer");         //Error handling
+    if (pclose(fp) == -1)
+        exitWithFailure("PCLOSE couldn´t close successfully");
 }
 
 
-//void md5sum_caller(char* path){
-//    pid_t pid = fork();
-//    if (pid==PID_ERROR){                                                        //Fork error handling
-//        perror("Fork call couldn´t be completed\n");
-//        exit(EXIT_FAILURE);
-//    } else if (pid==PARENT_PID){                                                //In the child process
-//        char* args[3]={"md5sum",path, NULL};                        //Every path is considered valid
-//        if (execve("./md5sum", args, NULL)==EXECVE_ERROR) {     //Execve error handling
-//            perror("Process md5sum couldn´t be executed");
-//            exit(EXIT_FAILURE);
-//        }
-//    } else{                                                                     //In the parent process
-//        int status;
-//        waitpid(pid, &status, 0);                                //Wait for md5sum to finish processing
-//    }
-//}
+
+static void exitWithFailure(const char *errMsg) {
+    perror(errMsg);
+    exit(EXIT_FAILURE);
+}
