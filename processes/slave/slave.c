757ee49f99b907b0d725ccce1ee9bd596cca9bab
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2024-04-09 18:40:01 -0300

    slave works with read and write

--- processes/slave/slave.c
+++ processes/slave/slave.c
@@ -2,15 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/select.h>
-
-//#define PID_ERROR (-1)
-//#define PARENT_PID 0                                             //The pid 0 refers to the parent process but the parent pid is stored by the child
-//#define EXECVE_ERROR (-1)
 
 #define ERROR (-1)
-
-#define BUFFER_SIZE 256                                             //The buffer size is bigger than necessary to avoid overflowing the buffer (It will be considered that every fgets will not reach the last character of the BUFFER_SIZE)
+#define BUFFER_SIZE 10000                                             //The buffer size is bigger than necessary to avoid overflowing the buffer (It will be considered that every fgets will not reach the last character of the BUFFER_SIZE)
+#define SMALL_BUFFER 100
 
 FILE *popen(const char *command, const char *type);                 //Must be included to avoid warnings
 int pclose(FILE *stream);
@@ -18,32 +13,28 @@ int pclose(FILE *stream);
 static void exitWithFailure(const char *errMsg);
 
 void md5sum_caller(char *path, char *buffer);
+void return_md5sum_result(char* md5sum_buffer);
+
 
 int main(int argc, char *argv[]) {
-    int eof_flag = 0;
-    fd_set fdSet;
-    while (!eof_flag) {                                                                              //The slave will process a number of files sent by the master one at the time, so with select I avoid busy waiting
-        FD_ZERO(&fdSet);
-        FD_SET(STDIN_FILENO, &fdSet);
-        int val = select(1, &fdSet, NULL, NULL, NULL);
-        if (val == ERROR)
-            exitWithFailure("SELECT didn´t work as expected");
-        if (val) {
-            char path[BUFFER_SIZE];
-            if (fgets(path, BUFFER_SIZE, stdin) == NULL) {
-                if (feof(stdin))
-                    eof_flag = 1;
-                else if (ferror(stdin))
-                    exitWithFailure("FGETS was unsuccessful to read the data into the buffer");
-            } else {
-                path[strlen(path)-1]='\0';
-                char buffer[BUFFER_SIZE] = {'\0'};
-                md5sum_caller(path, buffer);
-                buffer[strlen(buffer) -1] = '\0';                                      //I remove the newline created by fgets
-                printf("%s - PROCESS PID %d\n", buffer, getpid());
+    char filename_buffer[BUFFER_SIZE];
+    size_t bytes_read;
+    while ((bytes_read = read(STDIN_FILENO, filename_buffer, SMALL_BUFFER)) != 0) {
+        if (bytes_read==ERROR){
+            exitWithFailure("READ failed");
+        }
+        int position = 0;
+        for (int i = 0; i < bytes_read; i++) {
+            if (filename_buffer[i] == '\0' && filename_buffer[i-1]!='\0') {
+                char md5sum_buffer[SMALL_BUFFER];
+                md5sum_caller(&filename_buffer[position], md5sum_buffer);
+                position = i+1;
+                md5sum_buffer[strlen(md5sum_buffer)-1]='\0';
+                return_md5sum_result(md5sum_buffer);
             }
         }
     }
+    return 0;
 }
 
 void md5sum_caller(char *path, char *buffer) {
@@ -54,8 +45,7 @@ void md5sum_caller(char *path, char *buffer) {
                      "r");                              //Popen makes the fork call and creates the pipe, r means reading mode
     if (fp == NULL)                                                                  //check Popen failure
         exitWithFailure("POPEN couldn´t be created successfully");
-    if (fgets(buffer, BUFFER_SIZE, fp) ==
-        NULL)                            //Fgets fills the buffer with the md5sum result
+    if (fgets(buffer, BUFFER_SIZE, fp) ==NULL)                            //Fgets fills the buffer with the md5sum result
         exitWithFailure("FGETS was unsuccessful to read the data into the buffer");         //Error handling
     if (pclose(fp) == -1)
         exitWithFailure("PCLOSE couldn´t close successfully");
@@ -66,4 +56,10 @@ void md5sum_caller(char *path, char *buffer) {
 static void exitWithFailure(const char *errMsg) {
     perror(errMsg);
     exit(EXIT_FAILURE);
+}
+void return_md5sum_result(char* md5sum_buffer) {
+    size_t length = snprintf(NULL, 0, "%s - PROCESS PID %d\n", md5sum_buffer, getpid())+1;
+    char ret_md5sum[length];
+    snprintf(ret_md5sum, length, "%s - PROCESS PID %d\n", md5sum_buffer, getpid());
+    write(STDOUT_FILENO, ret_md5sum, length);
 }
