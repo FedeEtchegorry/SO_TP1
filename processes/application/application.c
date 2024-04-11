1ce2a7ae63a23714add3872c114517d7435ea823
Berni245 <bernizapico@gmail.com>
2024-04-11 03:42:38 -0300

    Fixed some logical problems but there's still some weird bug regarding the communication between app and slave

--- processes/application/application.c
+++ processes/application/application.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/select.h>
-#define __USE_XOPEN_EXTENDED
+#define __USE_XOPEN_EXTENDED // for ftruncate
+#include <semaphore.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <string.h>
-#include <semaphore.h>
 #include <unistd.h>
 #define SHM_SIZE 1024
 #define SHM_NAME "/results"
@@ -17,13 +17,12 @@
 #define CHILD 0
 #define FORK_QUANT 5
 typedef int pipe_t[2];
-enum{READ=0, WRITE=1};
+enum { READ = 0, WRITE = 1 };
 
-static void
-exitWithFailure(const char *errMsg); // can be a function for all files
+static void exitWithFailure(const char *errMsg); // can be a function for all files
 
 int main(int argc, char const *argv[]) {
-  argv++;
+  argv++; // así arranco por los paths
   int fileQuant = argc - 1;
   if (fileQuant < 1)
     exitWithFailure("No files were passed\n");
@@ -46,7 +45,8 @@ int main(int argc, char const *argv[]) {
   pipe_t sendTasks[FORK_QUANT];
   pipe_t getResults[FORK_QUANT];
 
-  for (int i = 0; i < FORK_QUANT; i++) {
+  int i;
+  for (i = 0; i < FORK_QUANT; i++) {
     if (pipe(sendTasks[i]) == ERROR)
       exitWithFailure("Error while creating pipe\n");
     if (pipe(getResults[i]) == ERROR)
@@ -61,58 +61,68 @@ int main(int argc, char const *argv[]) {
       dup(getResults[i][WRITE]);
       close(sendTasks[i][READ]);
       close(getResults[i][WRITE]);
-      for(int j = 0; j <= i; j++){
+      for (int j = 0; j <= i; j++) {
         close(sendTasks[j][WRITE]);
         close(getResults[j][READ]);
       }
 
-      char * argv[] = {NULL};
-      char * envp[] = {NULL};
+      char *argv[] = {SLAVE_CMD, NULL};
+      char *envp[] = {NULL};
       execve(SLAVE_CMD, argv, envp);
       exitWithFailure("Error while calling slave process\n");
-    }
-    else {
+    } else {
       close(sendTasks[i][READ]);
       close(getResults[i][WRITE]);
+      if(i < fileQuant)
+        write(sendTasks[i][WRITE], argv[i], strlen(argv[i]));
     }
   }
+  /*
+  file descriptors deberían quedar:
+  app = stdin, stdout, sendTasks[i][WRITE], getResults[i][READ] (i entre 0 y
+  FORK_QUANT - 1) s0 = sendTasks[0][READ], getResults[0][WRITE] s1 =
+  sendTasks[1][READ], getResults[1][WRITE] s2 = sendTasks[2][READ],
+  getResults[2][WRITE]
+  ...
+  s(FORK_QUANT - 1) = sendTasks[FORK_QUANT - 1][READ], getResults[FORK_QUANT -
+  1][WRITE]
+
+  la comunicación sería:
+  app por sendTask[i][WRITE] -> slave por sendTask[i][READ]
+
+  slave por getResults[i][WRITE] -> app por getResults[i][READ]
+  */
+
   fd_set rfds;
-  FD_ZERO(&rfds);
-  int count = 0;
   int maxFd = getResults[0][READ];
-  int i, j;
-  for (i = 0; i < FORK_QUANT && i < fileQuant; i++) { 
-    FD_SET(getResults[i][READ], &rfds);
-    maxFd = getResults[i][READ] > maxFd ? getResults[i][0] : maxFd;
+  char *iniAddress = buf;
+  int resultsCount = 0;
+  int j;
 
-    write(sendTasks[i][WRITE], argv[i], strlen(argv[i]) + 1);  
-  }
-
-  int inc;
-  char * iniAddress = buf;
-  
-  while (i < fileQuant) {
+  while (resultsCount < fileQuant) {
+    FD_ZERO(&rfds); // preparo el rfds en cada iteración ya que el select es destructivo
+    for (j = 0; j < FORK_QUANT; j++) {
+      FD_SET(getResults[j][READ], &rfds);
+      maxFd = getResults[j][READ] > maxFd ? getResults[j][READ] : maxFd;
+    }
+    FD_SET(READ, &rfds);
 
     select(maxFd + 1, &rfds, NULL, NULL, NULL);
 
-    for (j = 0; i < FORK_QUANT; j++) {
+    for (j = 0; j < FORK_QUANT; j++) { // cuando hay algo para leer, busco cuál es
       if (FD_ISSET(getResults[j][READ], &rfds)) {
-        
-        // en algún lado acá deberían ir semáforos creo
-        inc = read(getResults[j][READ], buf, SHM_SIZE - (buf - iniAddress));
-        if(inc > 0){
-          buf += inc;
-          count++;
+        buf += read(getResults[j][READ], buf, SHM_SIZE - (buf - iniAddress)); // lo mando al buffer y aumento el puntero
+        resultsCount++;
+        if(i < fileQuant){
+          write(sendTasks[i][WRITE], argv[i], strlen(argv[i]));
+          i++;
         }
-        write(sendTasks[i][WRITE], argv[i], strlen(argv[i]) + 1);
-        i++;
       }
-      FD_SET(getResults[j][0], &rfds);
     }
   }
-
-  for(i = 0; i < FORK_QUANT; i++){
-    close(sendTasks[i][WRITE]);
+  char endOfFile = EOF;
+  for (i = 0; i < FORK_QUANT; i++) { 
+    close(sendTasks[i][WRITE]); // closing the w-end, should lead to the r-end receiving EOF
     close(getResults[i][READ]);
   }
 
@@ -122,11 +132,11 @@ int main(int argc, char const *argv[]) {
   if (close(shmFd) == ERROR)
     exitWithFailure("Error while closing shm\n");
 
-  exit(0);
+  exit(EXIT_SUCCESS);
 }
 
 static void exitWithFailure(const char *errMsg) {
-  perror(errMsg);
+  fprintf(stderr, "%s", errMsg);
   exit(EXIT_FAILURE);
 }
 
