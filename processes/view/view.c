0755bf0c5242da68b357677765ce0039625ae12c
Franco Morroni <fmorroni99@gmail.com>
2024-04-15 18:36:50 -0300

    feat: continue working on view

--- processes/view/view.c
+++ processes/view/view.c
@@ -1,63 +1,72 @@
+#include <globals.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <unistd.h>
+#include <utils.h>
+
 #define PID_BUFFER 10
 #define ERROR (-1)
-#define RW_MODE 0666
+// #define RW_MODE 0666
+#define SHM_NAME_BUF_SIZE 100
+// #define RESULT_BUF_SIZE 100
 
-static void exitWithFailure(const char * errMsg); // can be a function for all files
-int app_process_exists();
+// int app_process_exists();
+int readFromShm(char* buf, sem_t* sem);
 
+int main(int argc, char* argv[]) {
+  // Lo hago con getchar para evitar tener que reemplazar el \n
+  char shmName[SHM_NAME_BUF_SIZE];
+  int shmNameLen = 0;
+  char c;
+  while ((c = getchar()) != '\n' && c != EOF) shmName[shmNameLen++] = c;
+  shmName[shmNameLen] = 0;
+  printf("From view shm name: %s\n", shmName);
 
+  int shmFd = safeShmOpen(shmName, O_RDONLY, 0 /* RW_MODE */);
+  // safeFtruncate(shmFd, SHM_SIZE);
+  char* const shmBuf = safeMmap(NULL, SHM_SIZE, PROT_READ, MAP_SHARED, shmFd, 0);
+  char* shmBufCurrent = shmBuf;
 
-int main(int argc, char* argv[]){
-   const int SIZE = 1024;
-   const char* name = "MD5_VIEWER";
+  printf("sem name: %s\n", SEM_NAME);
+  sem_t* enabledForRead = safeSemOpenRead(SEM_NAME);
 
-   int shm_fd;
-   char* ptr;
-   shm_fd = shm_open(name, O_RDONLY, RW_MODE );                                    //With this I create the shared memory. O_RDONLY means i will only read from a previous created shm
-   if (shm_fd==ERROR)                                                                       //Error handling
-       exitWithFailure("SHM_OPEN error");
-   if(ftruncate(shm_fd, SIZE) == -1)
-       exitWithFailure("FTRUNCATE error\n");
-   ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);      //mapping of the shared memory
-   if (ptr==(char*)-1)                                                                     //mapping error handling
-       exitWithFailure("MMAP error");
-   int i=0;
-   while((ptr[i]!='\0' || app_process_exists())&(i<SIZE)) {                                 //NEED SEMAPHORES!!!!
-        printf("%c", ptr[i++]);
-   }
-   if (shm_unlink(name)==ERROR)                                                             //Unlinkind from the share memory
-       exitWithFailure("SHM_UNLINK error");
-   return 0;
+  int resultLength = 0;
+  do {
+    printf("Reading result...\n");
+    resultLength = readFromShm(shmBufCurrent, enabledForRead);
+    shmBufCurrent += resultLength;
+  } while (resultLength > 0);
+  // while ((shmBuf[i] != '\0' || app_process_exists()) & (i < SIZE)) { // NEED SEMAPHORES!!!!
+  //   printf("%c", shmBuf[i++]);
+  // }
+
+  if (shm_unlink(shmName) == ERROR) perrorExit("shm_unlink() error on view");
+
+  return 0;
 }
 
-
-int app_process_exists(){                                                           //This allows to check if the app is still running
-    char line[PID_BUFFER];
-    FILE * command = popen("pidof ./app","r");
-    if (command==NULL){
-        exitWithFailure("POPEN error");
-    }
-    if (fgets(line,PID_BUFFER,command)==NULL){
-        exitWithFailure("FGETS error");
-    }
-    pid_t pid = atoi(line);
-    if (pid==0){
-        exitWithFailure("ATOI error");
-    }
-    if (pclose(command)==ERROR){
-        exitWithFailure("PCLOSE error" );
-    };
-    return pid;
+int readFromShm(char* buf, sem_t* sem) {
+  int len = 0;
+  while (buf[len] != '\n' && buf[len] != EOF) {
+    printf("%c", buf[len++]);
+  }
+  sem_wait(sem);
+  return buf[len] == EOF ? -1 : ++len;
 }
 
-static void exitWithFailure(const char * errMsg){
-    perror(errMsg);
-    exit(EXIT_FAILURE);
-}
+// int app_process_exists() { // This allows to check if the app is still running
+//   char line[PID_BUFFER];
+//   FILE* command = popen("pidof ./app", "r");
+//   if (command == NULL) {
+//     exitWithFailure("POPEN error");
+//   }
+//   if (fgets(line, PID_BUFFER, command) == NULL) {
+//     exitWithFailure("FGETS error");
+//   }
+//   pid_t pid = atoi(line);
+//   if (pid == 0) {
+//     exitWithFailure("ATOI error");
+//   }
+//   if (pclose(command) == ERROR) {
+//     exitWithFailure("PCLOSE error");
+//   };
+//   return pid;
+// }
